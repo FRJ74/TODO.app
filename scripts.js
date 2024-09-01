229af9adfee497e82ac9594aedaf79a0ac6d98d5
FRJ74 <frankr74@gmail.com>
2024-09-01 17:32:18 -0600

    Add the structure of the page

---
+++ scripts.js
@@ -0,0 +1,4 @@
+/* Access html element & declare variables */
+const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
