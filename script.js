a6345c20bf1eb307870f4e2af9c38be682fc1754
FRJ74 <frankr74@gmail.com>
2024-09-16 20:28:53 -0600

    Optimize base code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/* Access html element & declare variables */
 const taskForm = document.getElementById("task-form");
 const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 const openTaskFormBtn = document.getElementById("open-task-form-btn");
@@ -6,16 +5,10 @@ const closeTaskFormBtn = document.getElementById("close-task-form-btn");
 const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
 const cancelBtn = document.getElementById("cancel-btn");
 const discardBtn = document.getElementById("discard-btn");
-const tasksContainer= document.getElementById("tasks-container");
+const tasksContainer = document.getElementById("tasks-container");
 const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
-
-/*  
-Create an array that will
-store all the tasks along with their associated data,
-including title, due date, and description.
-*/
 
 const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
@@ -29,59 +22,61 @@ const addOrUpdateTask = () => {
     description: descriptionInput.value,
   };
 
-   if (dataArrIndex === -1) {
+  if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
   } else {
     taskData[dataArrIndex] = taskObj;
   }
 
   localStorage.setItem("data", JSON.stringify(taskData));
-
-  updateTaskContainer();
-  reset();
+  updateTaskContainer()
+  reset()
 };
 
 const updateTaskContainer = () => {
-  tasksContainer.innerHTML =  "" ;
+  tasksContainer.innerHTML = "";
+
   taskData.forEach(
-     ({ id, title, date, description }) => {
-         tasksContainer.innerHTML += `
-         <div class="task" id="${id}">
-           <p><strong>Title:</strong> ${title}</p>
-           <p><strong>Date:</strong> ${date}</p>
-           <p><strong>Description:</strong> ${description}</p>
-           <button onclick ="editTask(this)" type="button" class="btn">Edit</button>
-           <button onclick ="deleteTask(this)" type="button" class="btn">Delete</button>
-         </div>
-       `
-     }
-   ); 
- };
+    ({ id, title, date, description }) => {
+        tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
+        </div>
+      `
+    }
+  );
+};
 
- const deleteTask = (buttonEl) => {
-  const dataArrIndex = taskData.findIndex((item) => 
-    item.id === buttonEl.parentElement.id);
-    buttonEl.parentElement.remove();
-    taskData.splice(dataArrIndex, 1);
 
-    localStorage.setItem("data", JSON.stringify(taskData));
- };
-
- const editTask = (buttonEl) => {
+const deleteTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
-    (item) => item.id === buttonEl.parentElement.id);
-    currentTask = taskData[dataArrIndex];
-    currentTask.value = taskData[dataArrIndex];
-    titleInput.value = currentTask.title;
-    dateInput.value = currentTask.date;
-    descriptionInput.value = currentTask.description; 
+    (item) => item.id === buttonEl.parentElement.id
+  );
 
-    addOrUpdateTaskBtn.innerText = "Update Task";
-    taskForm.classList.toggle("hidden"); 
- };
+  buttonEl.parentElement.remove();
+  taskData.splice(dataArrIndex, 1);
+  localStorage.setItem("data", JSON.stringify(taskData));
+}
 
+const editTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
 
-/* Create a function that handles clearing the input fields. */
+  currentTask = taskData[dataArrIndex];
+
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
+
+  addOrUpdateTaskBtn.innerText = "Update Task";
+
+  taskForm.classList.toggle("hidden");  
+}
 
 const reset = () => {
   addOrUpdateTaskBtn.innerText = "Add Task";
@@ -96,43 +91,33 @@ if (taskData.length) {
   updateTaskContainer();
 }
 
-/* Opening and closing the modal */
-
-openTaskFormBtn.addEventListener("click", () => {
-    taskForm.classList.toggle("hidden");
-  });
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
 
 closeTaskFormBtn.addEventListener("click", () => {
-  const formInputsContainValues = titleInput.value ||
-   dateInput.value || descriptionInput.value !== "";
+  const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+  const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
 
-   const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
-
-   if (formInputsContainValues && formInputValuesUpdated) {
+  if (formInputsContainValues && formInputValuesUpdated) {
     confirmCloseDialog.showModal();
-    } else {
-      reset();
-    }; 
-   });
+  } else {
+    reset();
+  }
+});
 
-cancelBtn.addEventListener("click", () => {
-    confirmCloseDialog.close();
-  });
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
 
 discardBtn.addEventListener("click", () => {
-    confirmCloseDialog.close()
-    reset();
-  });
+  confirmCloseDialog.close();
+  reset()
+});
 
-  /* Get the values from the input fields; save them into the taskData array, and display them on the page. */
-  
-  taskForm.addEventListener("submit", (e) => {
-    e.preventDefault(); 
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    addOrUpdateTask();
-    
-   });
-
+  addOrUpdateTask();
+});
    
 
    
